0cdd65d541361dff6c72bd6f708c189ac642b604
mahmudsifat2793@gmail.com <mahmudsifat2793@gmail.com>
2026-04-17 08:44:49 +0600

    Remove unused files and add new Dart scripts for student info system, operators, and testing

---
+++ Dart/Module 1/operator.dart
@@ -0,0 +1,8 @@
+main() {
+  int a = 10;
+  int b = 3;
+  int sum = a + b;
+  // Integer division
+  print("Sum: $sum");
+  print("Integer Division: ${a ~/b}");
+}
---
+++ Dart/Module 1/test.dart
@@ -0,0 +1,5 @@
+main()
+{
+    String ? name = null;
+    print(name); 
+}
