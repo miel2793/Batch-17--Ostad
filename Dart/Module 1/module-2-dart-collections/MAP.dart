a8ef3c64554b6b59cb8b29add870588e217efe8b
mahmudsifat2793@gmail.com <mahmudsifat2793@gmail.com>
2026-06-25 03:20:17 +0600

    update  Map

---
+++ Dart/Module 1/module-2-dart-collections/MAP.dart
@@ -0,0 +1,13 @@
+import 'dart:io';
+main()
+{
+  Map<String,dynamic> Annex={
+    'Name':'Miel Mahmud Sifat',
+    'ID':20234103368,
+    'intake':52,
+    'Section':10
+
+  };
+  Annex ['Department']='CSE';
+  print(Annex['ID']);
+}
