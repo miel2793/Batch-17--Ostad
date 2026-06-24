23794e383c426d819d9cdcaa590633e12f61c1e8
mahmudsifat2793@gmail.com <mahmudsifat2793@gmail.com>
2026-06-25 01:48:41 +0600

    update

--- Dart/Module 1/list.dart
+++ Dart/Module 1/list.dart
@@ -1,4 +1,28 @@
-main() {
-  List<String> names = ["Alice", "Bob", "Charlie"];
-  print(names.first);
+main()
+{
+  List<String> fruits = ['Apple', 'Banana', 'Mango', 'Orange', 'Grapes'];
+  fruits.sort();
+for(var  s=0;s<fruits.length;s++)
+  {
+    print(fruits[s]);
+  }
+  fruits.add('Pineapple');
+  print(fruits);
+  fruits.remove('Banana');
+    print(fruits);
+fruits.addAll(['miel','anas']);
+print(fruits);
+fruits.insert(0, 'Strawberry');
+print(fruits);
+var addf=['kiwi','pear','watermelon'];
+fruits.insertAll(2, addf);
+print(fruits);
+fruits.removeAt(3);
+print(fruits);  
+fruits.removeLast();
+print(fruits);
+fruits.removeRange(1, 3);
+print(fruits); 
+fruits.replaceRange(1, 3, ['cherry','peach']);
+print(fruits);
 }
